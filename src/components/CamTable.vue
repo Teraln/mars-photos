496d4d53362bdbeca26b3eff994a5b3cbfe3c537
teraln <alen.terzic.fs@gmail.com>
2019-11-06 20:03:12 +0100

    Finished modal

---
+++ src/components/CamTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">Full</th>
+          <th class="text-left">Short</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in cameras" :key="item.name">
+          <td>{{ item.full }}</td>
+          <td>{{ item.short }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  name: "CamTable",
+  data() {
+    return {
+      cameras: [
+        { short: "FHAZ", full: "Front Hazard Avoidance Camera" },
+        { short: "RHAZ", full: "Rear Hazard Avoidance Camera" },
+        { short: "MAST", full: "Mast Camera" },
+        { short: "CHEMCAM", full: "Chemistry and Camera Complex" },
+        { short: "MAHLI", full: "Mars Hand Lens Imager" },
+        { short: "MARDI", full: "Mars Descent Imager" },
+        { short: "NAVCAM", full: "Navigation Camera" },
+        { short: "PANCAM", full: "Panoramic Camera" },
+        {
+          short: "MINITES",
+          full: "Miniature Thermal Emission Spectrometer (Mini-TES)"
+        }
+      ]
+    };
+  }
+};
+</script>
--- src/components/WelcomeModal.vue
+++ src/components/WelcomeModal.vue
@@ -12,7 +12,6 @@
 
         <!--Content-->
         <v-list three-line subheader>
-          <v-subheader>Sol</v-subheader>
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title>
@@ -24,49 +23,17 @@
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title>
-                <v-icon class="mr-1">mdi-information</v-icon>Rover cameras
+                <v-icon class="mr-1">mdi-information</v-icon>Rover Cameras
               </v-list-item-title>
-
-              <v-list-item-subtitle>FHAZ - Front Hazard Avoidance Camera</v-list-item-subtitle>
-              <v-list-item-subtitle>RHAZ - Rear Hazard Avoidance Camera</v-list-item-subtitle>
-              <v-list-item-subtitle>MAST - Mast Camera</v-list-item-subtitle>
-              <v-list-item-subtitle>CHEMCAM - Chemistry and Camera Complex</v-list-item-subtitle>
-              <v-list-item-subtitle>MAHLI - Front Hazard Avoidance Camera</v-list-item-subtitle>
-              <v-list-item-subtitle>MARDI - Front Hazard Avoidance Camera</v-list-item-subtitle>
-              <v-list-item-subtitle>NAVCAM - Front Hazard Avoidance Camera</v-list-item-subtitle>
-              <v-list-item-subtitle>PANCAM - Front Hazard Avoidance Camera</v-list-item-subtitle>
-              <v-list-item-subtitle>MINITES - Front Hazard Avoidance Camera</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
-        <v-list three-line subheader>
-          <v-subheader>General</v-subheader>
-          <v-list-item>
-            <v-list-item-action>
-              <v-checkbox v-model="notifications"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Notifications</v-list-item-title>
-              <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+              <CamTable />
             </v-list-item-content>
           </v-list-item>
           <v-list-item>
-            <v-list-item-action>
-              <v-checkbox v-model="sound"></v-checkbox>
-            </v-list-item-action>
             <v-list-item-content>
-              <v-list-item-title>Sound</v-list-item-title>
-              <v-list-item-subtitle>Auto-update apps at any time. Data charges may apply</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-action>
-              <v-checkbox v-model="widgets"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Auto-add widgets</v-list-item-title>
-              <v-list-item-subtitle>Automatically add home screen widgets</v-list-item-subtitle>
+              <v-list-item-title>
+                <v-icon class="mr-1">mdi-information</v-icon>Notes
+              </v-list-item-title>
+              <v-list-item-subtitle>Not all rovers have all listed cameras</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -76,9 +43,12 @@
 </template>
 
 <script>
+import CamTable from "./CamTable";
+
 export default {
   name: "WelcomeModal",
   props: ["modalState"],
+  components: { CamTable },
   data() {
     return {};
   },
